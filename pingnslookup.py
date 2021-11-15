8bdad4b7e4f47cd0149dadd9b9aaf9ab2ee78a20
pnaprasanna <pnaprasanna@gmail.com>
2021-11-15 18:01:02 +0530

    Added ping and nslookup

---
+++ pingnslookup.py
@@ -0,0 +1,23 @@
+import subprocess, socket
+
+title = "Hostname, Status, DNS"
+print(title)
+
+hostsfile=open("hosts", "r")
+lines=hostsfile.readlines()
+
+for line in lines:
+    response=subprocess.Popen(["ping",line.strip()], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = response.communicate()
+    #print(stdout)
+    #print(stderr)
+    
+    if (response.returncode == 0):
+        status = line.rstrip() + ", Online"
+    else:
+        status = line.rstrip() + ", Offline"
+    print(status)
+    
+    print(socket.gethostbyname(line.strip()))
+    
+    
