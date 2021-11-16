e6886c8a6f0665226ebe09e4b1dd8afe901c8c9b
pnaprasanna <pnaprasanna@gmail.com>
2021-11-16 21:47:18 +0530

    Ping and nslookup checkup for Linux servers
    Ping and nslookup checkup for Linux servers

---
+++ lpingnslookup.py
@@ -0,0 +1,25 @@
+import subprocess, socket
+
+title = "Hostname, Status, DNS"
+print(title)
+
+hostsfile=open("hosts", "r")
+lines=hostsfile.readlines()
+
+for line in lines:
+    response=subprocess.Popen(["ping", "-c1", line.strip()], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = response.communicate()
+    #print(stdout)
+    #print(stderr)
+    
+    if (response.returncode == 0):
+        status = line.rstrip() + ", Online,"
+    else:
+        status = line.rstrip() + ", Offline,"
+    #print(status)
+    try:
+       print status, socket.gethostbyname(line.strip())
+    except:
+       print("--")
+    
+    
