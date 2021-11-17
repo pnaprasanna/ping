990866b403523cc8da27dd9a6803eb4cd8b0c525
pnaprasanna <pnaprasanna@gmail.com>
2021-11-17 13:49:00 +0530

    Check status of ping and nslookup in Windows.
    Check status of ping and nslookup in Windows.

--- wpingnslookup.py
+++ wpingnslookup.py
@@ -7,17 +7,19 @@ hostsfile=open("hosts", "r")
 lines=hostsfile.readlines()
 
 for line in lines:
-    response=subprocess.Popen(["ping",line.strip()], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    response=subprocess.Popen(["ping", line.strip()], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, stderr = response.communicate()
     #print(stdout)
     #print(stderr)
     
     if (response.returncode == 0):
-        status = line.rstrip() + ", Online"
+        status = line.rstrip() + ", Online,"
     else:
-        status = line.rstrip() + ", Offline"
-    print(status)
-    
-    print(socket.gethostbyname(line.strip()))
+        status = line.rstrip() + ", Offline,"
+    #print(status)
+    try:
+       print(status, socket.gethostbyname(line.strip()))
+    except:
+       print(status, "--")
     
     
