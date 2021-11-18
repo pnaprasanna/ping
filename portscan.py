6f3c63ce6c89dd099d803e60562e87eab669a48f
pnaprasanna <pnaprasanna@gmail.com>
2021-11-18 21:27:56 +0530

    Portscan for single host
    Portscan for single host

---
+++ portscan.py
@@ -0,0 +1,20 @@
+#Python code for simple port scanning
+  
+import socket  #importing library 
+  
+ip = socket.gethostbyname (socket.gethostname())  #getting ip-address of host
+  
+for port in range(65535):      #check for all available ports
+  
+    try:
+   
+        serv = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # create a new socket
+  
+        serv.bind((ip,port)) # bind socket with address
+             
+    except:
+  
+        print(port, end =",") #print open port number
+
+ 
+    serv.close() #close connection
